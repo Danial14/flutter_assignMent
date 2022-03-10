335f99e89527698ecd824ee8cbda2a6821cb35f7
Danial <daniyalghani5@gmail.com>
2022-03-10 15:23:15 +0500

    flutter_start

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'App.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp();
+  Widget build(BuildContext buildContext){
+    return App();
+  }
+}
+
